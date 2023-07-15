50a35f09902a0417cf30ee1214dae0f4a5db579d
Irvin Dinkins <irvdude44@gmail.com>
2023-07-15 19:23:18 -0400

    passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    if length <= 0:
-        return "not valid number"
-    fib  = [0, 1]
-    if length <= 2:
-        return fib[:length]
-    for i in range(2, length):
-        fib.append(fib[i-1] + fib[i - 2])
-    return fib
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fib_sequence = [0, 1]
+        for i in range(2, length):
+            fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
+            
+        print(fib_sequence)
 
+# Test the function
+print_fibonacci(10)
