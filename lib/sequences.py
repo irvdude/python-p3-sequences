6b6aa07020d7029ac083c26b3f6db772a43d8ed0
Irvin Dinkins <irvdude44@gmail.com>
2023-07-15 18:49:47 -0400

    passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        return "not valid number"
+    fib  = [0, 1]
+    if length <= 2:
+        return fib[:length]
+    for i in range(2, length):
+        fib.append(fib[i-1] + fib[i - 2])
+    return fib
+
